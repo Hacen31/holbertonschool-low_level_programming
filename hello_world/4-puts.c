9453831cea13b73fa4e08e97359480ad449f0a9d
Hacen Kebdani <5017@holbertonschool.com>
2022-06-07 19:01:59 +0200

    pts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-gcc puts("Programming is like building a multilingual puzzle") >s<newline>0
+puts("Programming is like building a multilingual puzzle);
+    return (0);
 }
