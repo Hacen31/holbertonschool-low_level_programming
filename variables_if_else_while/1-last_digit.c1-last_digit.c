dfbecf3790872f12ef0e9b3322ccbcc9369409e8
Hacen Kebdani <5017@holbertonschool.com>
2022-06-09 21:48:16 +0200

    last

---
+++ variables_if_else_while/1-last_digit.c1-last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+int n;
+char (n > 5, n == 0, n < 6)
+	return (0)
+}
+
