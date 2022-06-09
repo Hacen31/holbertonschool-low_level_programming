d08f88e91052c0bb9996cf375a38b76806a8a566
Hacen Kebdani <5017@holbertonschool.com>
2022-06-09 20:29:40 +0200

    prt

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,17 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <time.h>
-
+/**
+* main - entry point
+* 
+* Return: Always 0 (Sucess)
+*/
 int main(void)
 {
 	char i;
 
-	for (i=a; i<z; i++)
-		putchar(i);
+	for (i = 'a'; i <= 'z'; i++)
+	putchar(i);
 	putchar(\n);
 
 	return (0);
