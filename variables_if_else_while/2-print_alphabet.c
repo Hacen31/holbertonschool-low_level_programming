9b66219136d8a4b343f9201904c9e41269161604
Hacen Kebdani <5017@holbertonschool.com>
2022-06-10 07:14:28 +0200

    prt

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -3,14 +3,14 @@
 # include <time.h>
 /**
 * main - entry point
+*
 * Return: Always 0 (Sucess)
 */
 int main(void)
-{
-char i;
-for (i = 'a'; i <= 'z'; \n ; i++)
-putchar(i);
-putchar(\n);
-
-return (0);
-}
+		{
+			char i;
+			for (i = 'a'; i <= 'z'; i++)
+					putchar(i);
+			putchar(\n)
+				return (0);
+		 }
