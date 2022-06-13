67c76e3697588cec06ba078e605221ae853f469f
Hacen Kebdani <5017@holbertonschool.com>
2022-06-13 13:49:21 +0200

    alp

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,12 +5,11 @@
  * je veux afficher l'alphabet
  * Return: Always 0.
 */
-int void print_alphabet(void)
+void print_alphabet(void)
 {
 	int a;
 
-	for (a = "a"; a = "z"; a, a++)
+	for (a = 'a'; a <= 'z'; a++)
 		_putchar(a);
-	_putchar("\n");
-return (0);
+	_putchar('\n');
 }
