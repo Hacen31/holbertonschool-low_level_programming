35d53319de112728241035a887a4dfeb12fc611c
Hacen Kebdani <5017@holbertonschool.com>
2022-06-13 11:46:45 +0200

    pt

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,22 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
 #include "main.h"
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-char c;
-int n;
-for (n = 0; n < 10; n++)
-_putchar((n % 10) + '0');
-for (c = 'a'; c <= 'f'; c++)
-_putchar(c);
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
