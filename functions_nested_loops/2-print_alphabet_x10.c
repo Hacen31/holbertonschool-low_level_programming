c5826a07193b88fa6f39406d5762331062ddc37a
Hacen Kebdani <5017@holbertonschool.com>
2022-06-13 15:54:46 +0200

    app

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,9 @@ void print_alphabet_x10(void)
 	int a;
 	char b;
 
-	for (a = 0; a <= 10; ++a)
-
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = 'a'; a <= 'z' ; b++)
+		for (b = 'a'; a <= 'z'; b++)
 		{
 			_putchar(b);
 		}
