2a45052126640a75f2cdb695433f5c7b9e7d14eb
Hacen Kebdani <5017@holbertonschool.com>
2022-06-13 14:01:53 +0200

    fix exo 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
-#include "main.c"
 #include "main.h"
 /**
  * void print_alphabet(void); - check the code
  * je veux afficher l'alphabet
  * Return: Always 0.
-*/
+ */
 void print_alphabet(void)
 {
 	char a;
