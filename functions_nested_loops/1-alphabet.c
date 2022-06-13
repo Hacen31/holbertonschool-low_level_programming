873cdadca37d80c7621d69d39b4110a6d885013d
Hacen Kebdani <5017@holbertonschool.com>
2022-06-13 13:19:05 +0200

    ptc

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.c"
+#include "main.h"
+/**
+ * void print_alphabet(void); - check the code
+ * je veux afficher l'alphabet
+ * Return: Always 0.
+*/
+int void print_alphabet(void)
+{
+		  int a;
+		   for (a = "a"; a = "z"; a, a++)
+				_putchar(a);
+			_putchar("\n");
+			 return (0);
+	  }
