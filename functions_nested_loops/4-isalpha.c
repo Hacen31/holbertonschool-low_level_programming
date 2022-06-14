3a3bbcca5703cf82653cc6e0705a966d395598bc
Hacen Kebdani <5017@holbertonschool.com>
2022-06-14 16:20:24 +0200

    isa

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,19 +1,22 @@
 #include "main.h"
-#include <stdlib.h>
 /**
  * _isalpha - Cfr description
  * @c: input character
  * Description: checks for lower case character
  * Return: 1 if is lowercase and 0 otherwise
-*/
+ */
 int _isalpha(int c)
 {
-char i;
-int b = 0;
-for (i = 'a'; i <= 'z'; i++)
-{
-if (c == i)
-b = 1;
-}
-return (b);
+	char i, h;
+	int b = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		for (h = 'A'; h <= 'Z'; h++)
+		{
+			if ((c == i) || (c == h))
+				b = 1;
+		}
+	}
+	return (b);
 }
