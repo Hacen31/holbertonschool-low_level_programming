750b7ba8ed3b0ce229a64305bf4e9ba48b60cd4d
Hacen Kebdani <5017@holbertonschool.com>
2022-06-15 11:53:31 +0200

    tim

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -13,19 +13,14 @@ void times_table(void)
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-
 		for (mult = 1; mult <= 9; mult++)
 		{
 			prod = num * mult;
-
 			if (prod <= 9)
 				_putchar(' ');
-
 			else
 				_putchar((prod / 10) + '0');
-
 			_putchar((prod % 10) + '0');
-
 			if (mult == 9)
 				continue;
 			_putchar('\n');
