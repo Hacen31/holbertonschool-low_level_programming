17d669eb7fe6b33bd5e98278172ca3c37f731f06
Hacen Kebdani <5017@holbertonschool.com>
2022-06-16 11:04:26 +0200

    more

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_isupper - Cfr description
+ * @c: input character
+ * Description: checks for lower case character
+ * Return: 1 if is lowercase and 0 otherwise
+ */
+
+int _isupper(int c)
+
+{
+	char i;
+	int b = 0;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		if (c == i)
+			b = 1;
+	}
+	return (b);
+}
