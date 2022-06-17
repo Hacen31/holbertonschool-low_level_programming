27189747d6fe7cb1b9985f9890e74cbf3cd26da4
Hacen Kebdani <5017@holbertonschool.com>
2022-06-17 18:04:28 +0200

    res

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,13 @@
 #include "main.h"
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - check the code
- * Return: Always 0.
+ * reset_to_98 - check the code\
+ * @n: variable
 */
 
-int main(void)
+void reset_to_98(int *n)
 {
 	int n;
 	*n = 98;
-	return (0);
 }
