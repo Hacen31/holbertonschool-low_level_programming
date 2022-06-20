2390a5d59432921d774bd83da492ef2d3364c23a
Hacen Kebdani <5017@holbertonschool.com>
2022-06-20 18:54:10 +0200

    rcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -3,8 +3,8 @@
 
 /**
  * strcat - Concatenates the pointed string and the end byte
- * null end byte pointed to by @dest.
- * @dest: A pointer to the string to be concatenated upon.
+ * char *strcat(char *dest, const char *src).
+ *
  * @src: The source string to be appended to @dest.
  *
  * Return: A pointer to the destination string @dest.
