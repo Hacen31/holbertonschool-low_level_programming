90503a57a8243215b0e569b2f8ed876d38a6ee5c
Hacen Kebdani <5017@holbertonschool.com>
2022-06-20 11:07:15 +0200

    srcat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - appends src to the dest string
+ * @dest: string to append by src
+ * @src: string to append to dest
+ * @n: largest number of bytes to append
+ * Return: address of dest
+ */
+
+char *_strncat(char *dest, char *src, int n);
+{
+	int a, b;
+
+	a = b = 0;
+	while (*(dest + a))
+		a++;
+	while ((*(dest + a) = *(src + b)))
+	{
+		a++;
+		b++;
+	}
+	return (dest);
+}
