089cc18774d734cde2648be7de6289f51db19d88
Hacen Kebdani <5017@holbertonschool.com>
2022-06-20 17:19:48 +0200

    add

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,19 +1,18 @@
-#include <stdio.h>                                                                                  
+#include "main.h"
+#include <stdio.h>
 
-/**                                                                                                 
- * main - debugging example                                                                         
- * Return: 0                                                                                        
- */                                                                                                 
-int main(void)                                                                                      
-{                                                                                                   
-	char *hello = "Hello, World!";                                                              
+/**
+ * add - Additionne deux nombres entiers.
+ * @num1 : Le premier entier à ajouter.
+ * @num2 : deuxième entier à ajouter.
+ *
+ * Retour : Le résultat de l'addition.
+ *
+ */
+int add(int, int);
+{
+	int n;
+	int  add ( int num1, int num2)
 
-	for (i = 0; hello[i] != '\0'; i++)                                                          
-	{                                                                                           
-		printf("%c", hello[i]);                                                             
-	}                                                                                           
-
-	printf("\n");                                                                               
-
-	return (0);                                                                                 
-}                
+		retour (num1 + num2);
+}
