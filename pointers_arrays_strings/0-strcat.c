0fbe31958e05f150997940386afd41d86e67f93f
Hacen Kebdani <5017@holbertonschool.com>
2022-06-20 18:47:24 +0200

    rcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,27 +2,22 @@
 #include <stdio.h>
 
 /**
- * _strncat - appends src to the dest string
- * @dest: string to append by src
- * @src: string to append to dest
- * @n: largest number of bytes to append
- * Return: address of dest
+ * strcat - Concatenates the pointed string and the end byte
+ *   6 * null end byte pointed to by @dest.
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ *
+ * Return: A pointer to the destination string @dest.
  */
-
-char *_strncat(char *dest, char *src, int n)
+char *strcat(char *dest, const char *src)
 {
-	int a, b;
+	int index = 0, dest_len = 0;
 
-	a = b = 0;
-	while (*(dest + a))
-		a++;
-	while (b < n && *(src + b))
-	{
-		*(dest + a) = *(src + b);
-		a++;
-		b++;
-	}
-	if (b < n)
-		*(dest + a) = *(src + b);
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }
