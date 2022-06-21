d58b461f2742f3e969152bce4f7c71d427860692
Hacen Kebdani <5017@holbertonschool.com>
2022-06-21 14:56:19 +0200

    cats

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,20 +2,27 @@
 #include <stdio.h>
 
 /**
- * strcat - Concatène la chaîne pointée et l'octet de fin.
+ * _strcat - Concatène la chaîne pointée et l'octet de fin.
  * @dest: ajoute la chaine source src
  * @src: Chaine de caractere source
  * Return: renvoie un pointeur vers la chaîne résultante dest
- */
+*/
+
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int index, dest_len;
 
-	while (dest[index++])
+index = 0;
+	dest_len = 0;
+	/* Ca c'est mon compteur */
+	while (dest[dest_len])
 		dest_len++;
 
+	/* Mon compteur est a 6 */
 	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index++];
-
+	{
+		dest[dest_len] = src[index];
+		dest_len++;
+	}
 	return (dest);
 }
