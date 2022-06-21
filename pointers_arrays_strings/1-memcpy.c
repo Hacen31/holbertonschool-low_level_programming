ae240f6e4c7ec6f01fc4c0269d408f58624f7a9d
Hacen Kebdani <5017@holbertonschool.com>
2022-06-21 15:41:45 +0200

    memc

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+#include "main.h"
+
+/**
+ * _memcpy - copy memory area
+ * @dest: dest memory area
+ * @src: source memory area
+ * @n: bytes from memory area to copy
+ *
+ * Return: memory area replaced
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	while (n != 0)
+	{
+		n--;
+		dest[n] = src[n];
+	}
+	return (dest);
+}
