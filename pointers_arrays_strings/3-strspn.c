68f1af38eeb6db81ae8ff972750064e6393b6420
Hacen Kebdani <5017@holbertonschool.com>
2022-06-23 15:16:16 +0200

    sp

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,35 +1,32 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _strspn - Gets the length of a prefix of a string.
- * _strspn - Gets the length of a prefix substring.
- * @s: The string to be searched.
- * @accept: The prefix to be measured.
+ * _strspn - search a string for a set of bytes
+ * @s: source string
+ * @accept: accepted string
  *
- * only of bytes from accept.
- * Return: The number of bytes in s which
- * consist only of bytes from accept.
+ * Return: number of bytes in the init segment
  */
-
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int bytes = 0;
-	int index = 0;
-	int index;
+	unsigned int a = 0, b, d = 0;
 
-while ((*s >= 'a' && *s <= 'z') || (*s >= 'A' && *s <= 'Z'))
-while (*s)
-{
-for (index = 0; accept[index]; index++)
-{
-if (*s == accept[index])
-{
-bytes++;
-break;
-}
-else if (!(accept[index + 1])
-return (bytes++)
-}
-}
+	while (accept[a])
+	{
+		b = 0;
+
+		while (s[b] != 32)
+		{
+			if (accept[a] == s[b])
+			{
+				d++;
+			}
+
+			b++;
+		}
+
+		a++;
+	}
+
+	return (d);
 }
