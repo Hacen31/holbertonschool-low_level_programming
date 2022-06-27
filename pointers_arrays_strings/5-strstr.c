4df9837d4f6636cc0adc68d0cc7ce640a63feb10
Hacen Kebdani <5017@holbertonschool.com>
2022-06-27 10:47:41 +0200

    str

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -6,8 +6,6 @@
  * @haystack: The string to be searched.
  * @needle: The substring to be located.
  *
- * Return: If the substring is located - a pointer to the beginning
- * of the located substring.
  * If the substring is not located - NULL.
  */
 
