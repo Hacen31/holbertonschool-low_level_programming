74de17d665f70835f0d26d61c72e3eb0a3746c91
Hacen Kebdani <5017@holbertonschool.com>
2022-06-29 18:49:12 +0200

    add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -7,30 +7,29 @@
  * @argc: The number of arguments passed to the program.
  * @argv: An array of pointers to the arguments.
  *
- * Return: If the numbers contain only digits - 0.
- * If one of the numbers contains non-digits - 1.
+ * Return: If one of the numbers contains symbols that are non-digits - 1.
+ * Otherwise - 0.
  */
 
 int main(int argc, char *argv[])
 {
-	int num, sum = 0;
+	int num, digit, sum = 0;
 
 	for (num = 1; num < argc; num++)
 	{
-		if (atoi(argv[num]) != 0)
-			sum += atoi(argv[num]);
-
-		else
+		for (digit = 0; argv[num][digit]; digit++)
 		{
-			printf("Error\n");
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 
-			return (1);
-			sum += atoi(argv[num];
+		sum += atoi(argv[num]);
+	}
 
-					}
-					}
-					printf("%d\n", sum);
+	printf("%d\n", sum);
 
-					return (0);
-					}
+	return (0);
 }
