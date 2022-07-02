e24303980bdbf398a2ed741eedc1be8b25b82410
Hacen Kebdani <5017@holbertonschool.com>
2022-07-02 21:10:55 +0200

    include

--- preprocessor/0-object_like_macro.h
+++ preprocessor/0-object_like_macro.h
@@ -1,9 +1,8 @@
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
 
-/**
+/*
  * File: 0-object_like_macro.h
- * Auth: Brennan D Baraban
  * Desc: Header file that defines a macro named
  * SIZE as an abbreviation for the token 1024.
  */
@@ -14,4 +13,3 @@
 #define SIZE 1024
 
 #endif
-
