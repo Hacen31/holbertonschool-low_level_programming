4c4fed1f2227ac225001551f8d19ce4ca646f65b
Hacen Kebdani <5017@holbertonschool.com>
2022-07-02 21:50:31 +0200

    pi

--- preprocessor/1-pi.h
+++ preprocessor/1-pi.h
@@ -5,7 +5,6 @@
 
 /*
  * File: 1-pi.h
- * Auth: Brennan D Baraban
  * Desc: Header file that defines a macro named PI
  * as an abbreviation for the token 3.14159265359.
  */
@@ -13,3 +12,4 @@
 #define PI 3.14159265359
 
 #endif
+
