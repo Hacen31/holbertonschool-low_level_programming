06dc07cda31897540d7e41599c212a5a3fab53fb
Hacen Kebdani <5017@holbertonschool.com>
2022-07-02 21:33:34 +0200

    sum

---
+++ preprocessor/4-sum.h
@@ -0,0 +1,16 @@
+#ifndef 4_SUM_H
+#define 4_SUM_H
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ * that computes the sum of the numbers x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
+
