93a1e2d8dea05e177b6e0fde575adf27cab71727
Hacen Kebdani <5017@holbertonschool.com>
2022-07-03 21:31:37 +0200

    string

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -4,3 +4,8 @@ void    ft_putnbr(int nombre);
 int     ft_strcmp(char *s1, char *s2);
 int     ft_strlen(char *str);
 void    ft_swap(int *a, int *b);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+struct dog_t *new_dog(char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
