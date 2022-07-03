570bd3b1587f978449431603cb179bc56ad4c1f9
Hacen Kebdani <5017@holbertonschool.com>
2022-07-03 11:24:15 +0200

    object

--- preprocessor/0-object_like_macro.h
+++ preprocessor/0-object_like_macro.h
@@ -1,11 +1,12 @@
 #ifndef MACRO_SIZE
 #define MACRO_SIZE 1024
 
+/* Size of array */
+
 #define _XTAL_FREQ 1000000
 #define input LATC0
 #define output LATC1
-#define size 1024       /* Size of array */
+#define size 1024
 
-extern const int data[size 1024];
-
+extern const int data[size];
 #endif
