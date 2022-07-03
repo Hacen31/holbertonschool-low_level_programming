8f4684e6cfe9e13452feb4b285b0ee7537d7d22c
Hacen Kebdani <5017@holbertonschool.com>
2022-07-03 19:10:40 +0200

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -12,7 +12,7 @@
  * @owner: The owner of the dog.
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
