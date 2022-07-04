cb579441925504e241b8ad540066563a73cdb4e9
Hacen Kebdani <5017@holbertonschool.com>
2022-07-04 13:56:21 +0200

    nameprint

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -10,3 +10,7 @@ void free_dog(dog_t *d);
 struct dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 struct dog my_dog;
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
