bac7b9917055736b8038c5d2118cdb11216c8c04
Hacen Kebdani <5017@holbertonschool.com>
2022-07-04 12:38:33 +0200

    dg

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -5,7 +5,8 @@
  * struct dog - Dog attributes
  * @name: The name of the dog.
  * @age: The age of the dog.
- * @owner: The owner of the dog.  
+ * @owner: The owner of the dog.
+ *
  * Description: The attributes of a dog.
  */
 struct dog
