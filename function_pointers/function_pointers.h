5278acffc2087b97e589efab31f688743c2a5ed4
Hacen Kebdani <5017@holbertonschool.com>
2022-07-05 15:24:09 +0200

    itera

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-#include <stddef>
+#include <stddef.h>
 void print_name(char *name,void (*f)(char *));
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
+int _putchar(char c);
 #endif /* FUNCTION_POINTERS_H */
