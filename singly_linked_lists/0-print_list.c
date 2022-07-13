50e8a869fe88710d5647fd337987b577fc936c42
Hacen31 <kebdanihacen551967@gmail.com>
2022-07-13 19:20:32 +0200

    liste

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -4,12 +4,28 @@
 #include "lists.h"
 
 /**
- * structure list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
+ * afficher list - afficher tout les elements de list_t list.
+ * @:list_t liste
  */
 
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
 const list_t *h
 {
 	struct list_head *next;
