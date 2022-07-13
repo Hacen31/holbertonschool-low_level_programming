617c335bf1d2ee51d2316bcc8386b9dd57ee1861
Hacen31 <kebdanihacen551967@gmail.com>
2022-07-13 18:30:58 +0200

    li

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * structure list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ */
+
+list_head
+{
+	struct list_head *next;
+	struct list_head *prev;
+
+	list_t;
+	return (0);
+}
