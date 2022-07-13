79b75d123b2a40d69e334f7641ac70435fcacaec
Hacen31 <kebdanihacen551967@gmail.com>
2022-07-13 18:43:41 +0200

    liste

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef _LIST_T_H
+#define _LIST_T_H 1
+
+int _putchar(char c);
+typedef struct list_s;
+size_t print_list(const list_t *h);
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+}   list_t;
+
+#enfif /* list_t.h */
