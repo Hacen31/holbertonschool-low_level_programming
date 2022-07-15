b993e68c163aaef3e4156d35407bc5f2d7b9bf16
Hacen31 <kebdanihacen551967@gmail.com>
2022-07-15 18:16:53 +0200

    end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -16,29 +16,23 @@ list_t *add_node_end(list_t **head, const char *str)
 	int len;
 	list_t *new, *last;
 
-
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-
 
 	dup = strdup(str);
 	if (str == NULL)
 		return (NULL);
 
-
 	for (len = 0; str[len];)
 		len++;
-
 
 	new->str = dup;
 	new->len = len;
 	new->next = NULL;
 
-
 	if (*head == NULL)
 		*head = new;
-
 
 	else
 	{
@@ -48,7 +42,5 @@ list_t *add_node_end(list_t **head, const char *str)
 		last->next = new;
 	}
 
-
 	return (*head);
 }
-
