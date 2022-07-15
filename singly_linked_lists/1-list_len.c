4cf67d9b117ae39ea0e193380da8715e6e99c1ce
Hacen31 <kebdanihacen551967@gmail.com>
2022-07-15 11:32:10 +0200

    len

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+
+/**
+ * list_len - Trouve le nombre d'éléments dans
+ * une liste_t liée.
+ * @h : La liste_t liée.
+ *
+ * Retourner : Le nombre d'éléments dans h.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+
+	while (h)
+	{
+		elements++;
+		h = h->next;
+	}
+
+
+	return (elements);
+}
