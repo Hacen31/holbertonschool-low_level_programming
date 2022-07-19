a8983ee25d5b3768a886c2d06d226842ccaef4a1
Hacen31 <kebdanihacen551967@gmail.com>
2022-07-19 20:29:17 +0200

    bine

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -35,7 +35,7 @@ int len;
 			if (b == '\0')
 				return (0);
 
-			for (len = 0; b[len];)
+			for (len = 0; b[len])
 				len++;
 
 			for (len -= 1; len >= 0; len--)
@@ -47,6 +47,6 @@ int len;
 				mult *= 2;
 			}
 
-			if (!b)
+			 if (b == '\0')
 				return (0);
 }
