ce3d52d33574ab6ac0dce7b244f07c0b77c0238f
Hacen31 <kebdanihacen551967@gmail.com>
2022-07-19 21:43:02 +0200

    flip

--- bit_manipulation/5-flip_bits.c5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c5-flip_bits.c
@@ -6,7 +6,6 @@
  * flipped to get from one number to another.
  * @n: The number.
  * @m: The number to flip n to.
- *
  * Return: The necessary number of bits to flip to get from n to m.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
