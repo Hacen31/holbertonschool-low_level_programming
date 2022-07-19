49f8324feaa7b6d39e32376911737f01e5d06c1e
Hacen31 <kebdanihacen551967@gmail.com>
2022-07-19 20:12:12 +0200

    bine

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,34 +1,4 @@
- #include <stdio.h>
-  2 #include "main.h"
-  3
-  4 /**
-  5  * binary_to_uint - Converts a binary number to an unsigned int.
-  6  * @b: A pointer to a string of 0 and 1 chars.
-  7  *
-  8  * Return: If b is NULL or contains chars not 0 or 1 - 0.
-  9  * Otherwise - the converted number.
- 10  */
- 11 unsigned int binary_to_uint(const char *b)
- 12 {
- 13     unsigned int num = 0, mult = 1;
- 14     int len;
- 15
- 16     if (b == '\0') #include <stdio.h>
-  2 #include "main.h"
-  3
-  4 /**
-  5  * binary_to_uint - Converts a binary number to an unsigned int.
-  6  * @b: A pointer to a string of 0 and 1 chars.
-  7  *
-  8  * Return: If b is NULL or contains chars not 0 or 1 - 0.
-  9  * Otherwise - the converted number.
- 10  */
- 11 unsigned int binary_to_uint(const char *b)
- 12 {
- 13     unsigned int num = 0, mult = 1;
- 14     int len;
- 15
- 16     if (b == '\0')#include <stdio.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -38,26 +8,45 @@
  * Return: If b is NULL or contains chars not 0 or 1 - 0.
  * Otherwise - the converted number.
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
+unsigned int num = 0, mult = 1;
 	int len;
 
 	if (b == '\0')
-		return (0);
 
-	for (len = 0; b[len];)
-		len++;
+#include <stdio.h>
+#include "main.h"
 
-	for (len -= 1; len >= 0; len--)
-	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: A pointer to a string of 0 and 1 chars.
+ *
+ * Return: If b is NULL or contains chars not 0 or 1 - 0.
+ * Otherwise - the converted number.
+ */
 
-		num += (b[len] - '0') * mult;
-		mult *= 2;
-	}
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int num = 0, mult = 1;
+int len;
 
-if (!b)
-	return (0);
+			if (b == '\0')
+				return (0);
+
+			for (len = 0; b[len];)
+				len++;
+
+			for (len -= 1; len >= 0; len--)
+			{
+				if (b[len] != '0' && b[len] != '1')
+					return (0);
+
+				num += (b[len] - '0') * mult;
+				mult *= 2;
+			}
+
+			if (!b)
+				return (0);
 }
