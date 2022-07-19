333ee77e5d4aa295e5f0aa9fb0b5cbce8d1cdb69
Hacen31 <kebdanihacen551967@gmail.com>
2022-07-19 10:48:51 +0200

    printall

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -7,9 +7,7 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-char *symbol
-void (*print)(va_list arg)
-struct printer	
+void (*print)(va_list arg);
 
 
 #endif /* VARIADIC_FUNCTIONS_H */
